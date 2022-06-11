0c3c252cabbcd77e75b9807387e362788a1f239c
Carlos Jimenez <cjimenezb@est.utn.ac.cr>
2022-06-11 16:12:33 -0600

    Se agregó la función de filtrado a las fotos

--- src/components/Album/Album.js
+++ src/components/Album/Album.js
@@ -6,41 +6,65 @@ import "./Album.css"
 const Album = () => {
 
     const {albumId} = useParams();
+    const [name, setName] = useState('');
     const [photos, setPhotos] = useState([]);
+    const [filteredPhotos, setFilteredPhotos] = useState([]);
 
 
   useEffect(() => {
     axios.get("https://jsonplaceholder.typicode.com/photos").then((res) => {
       const { data } = res;
 
-      const albumPhotos = data.filter(photo => photo.albumId.toString() === albumId);
-      setPhotos(albumPhotos);
+      const userPhotos = data.filter(photo => photo.albumId.toString() === albumId);
+      setPhotos(userPhotos);
+      setFilteredPhotos(userPhotos);
 
     });
   },[albumId]);
 
-    return (
-      <div className="container">
-        <input
-          type="search"
-          className="input"
-          placeholder="Filter photos by title"
-        />
+  const filter = (e) => {
 
-        <div className="photo-list">
-          {photos && photos.length > 0 ? (
-            photos.map((photo) => (
-              <div key={photo.id} className="photo-array">
-                <img className="photo" src={ photo.thumbnailUrl } title={ photo.title } alt={ photo.title } />
-                <p className="photo-id">{photo.title}</p>
-              </div>
-            ))
-          ) : (
-            <h1>No results found!</h1>
-          )}
-        </div>
+    const keyword = e.target.value;
+    if(keyword===''){
+      setPhotos(photos)
+    }else if (keyword !== '') {
+      const results = photos.filter((photo) => {
+        return photo.title.toLowerCase().includes(keyword.toLowerCase());
+        // Use the toLowerCase() method to make it case-insensitive
+      });
+      setFilteredPhotos(results);
+    
+      // If the text field is empty, show all users
+    }
+
+    setName(keyword);
+
+  }
+
+  return (
+    <div className="container">
+      <input
+        type="search"
+        value={name}
+        onChange={filter}
+        className="input"
+        placeholder="Filter photos by title"
+      />
+
+      <div className="photo-list">
+        {filteredPhotos && filteredPhotos.length > 0 ? (
+          filteredPhotos.map((photo) => (
+            <div key={photo.id} className="photo-array">
+              <img className="photo" src={ photo.thumbnailUrl } title={ photo.title } alt={ photo.title } />
+              <p className="photo-id">{photo.title}</p>
+            </div>
+          ))
+        ) : (
+          <h1>No results found!</h1>
+        )}
       </div>
-    );
+    </div>
+  );
 };
 
 export default Album;
